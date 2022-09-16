a971c56857e6b463b070c458264746bb9bece369
KajalShendage <shendagekajal123@gmail.com>
2022-09-16 20:58:43 +0530

    [Pramod] Add: To Find Frequency Of Words

--- NonLinearDataStructureProblem/Program.cs
+++ NonLinearDataStructureProblem/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to the NonLinear Data Structure Problems!");
+﻿using NonLinearDataStructureProblem;
+
+MyMapNode<string, int> hash = new MyMapNode<string, int>(6);
+string words = "to be or not to be";
+string[] arr = words.Split(' ');
+LinkedList<string> checkForDuplication = new LinkedList<string>();
+   foreach (string element in arr)
+   {
+       int count = 0;
+       foreach (string match in arr)
+       {
+           if (element == match)
+           {
+               count++;
+               if (checkForDuplication.Contains(element))
+               {
+                   break;
+               }
+           }
+       }
+       if (checkForDuplication.Contains(element))
+       {
+           continue;
+       }
+       checkForDuplication.AddLast(element);
+       hash.Add(element, count);
+   }
+   Console.WriteLine("Frequency of the word");
+   hash.Display();
